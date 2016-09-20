dd0c477742582efb47e276f30651175a3fb1ea71
vnatco <contact@vnat.co>
2016-09-20 13:31:25 -0400

    Added license notes at the top

--- js/jQuery.easing.js
+++ js/jQuery.easing.js
@@ -1,3 +1,12 @@
+/*
+ * jQuery Easing v1.3.2 - http://gsgd.co.uk/sandbox/jquery/easing/
+ * Open source under the BSD License.
+ * Copyright © 2008 George McGinley Smith
+ * All rights reserved.
+ * https://raw.github.com/gdsmith/jquery-easing/master/LICENSE
+*/
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
 (function($){$.easing['jswing'] = $.easing['swing'];
 
 $.extend( $.easing,
